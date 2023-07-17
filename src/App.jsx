d36942bfc8336848579f0bb593bb0fbb9d455d9b
KingKiarie <kiarie7mbugua@gmail.com>
2023-07-17 10:21:36 +0300

    :/

--- src/App.jsx
+++ src/App.jsx
@@ -53,14 +53,14 @@ function App() {
   
     <>
     <div className='font-serif text-lg text-slate-400'><h2>Kevin's Calculater App</h2></div>
-    <div className='w-2/6  mx-8 w-fit h-screen flex flex-wrap justify-center items-center bg-cyan-700'>
+    <div className='w-2/6  w-fit h-screen flex flex-wrap justify-center items-center bg-cyan-700'>
       <div>
-        <div className="w-5/6  flex justify-center align-center h-5/6 bg-gray-800">
+        <div className="w-6/6  flex justify-center align-center h-5/6 bg-gray-800">
           <div className='flex w-4/6 h-3/6 text-white justify-end pr-10 text-3x1 mt-4'>
             {num}
           </div>
         </div>
-        <div className="w-4/6  flex flex-wrap items-center justify-center text-white space-x-0.5  ">
+        <div className="w-4/6  flex flex-wrap items-center justify-center text-white space-x-0.5 mx-20 ">
           <button onClick={clear} className='mt-5 m-2 w-14 h-14 flex items-center justify-center bg-orange-500 rounded-lg'>c</button>
           <button className='mt-5 m-2 w-14 h-14 flex items-center justify-center bg-orange-500 rounded-lg'>(</button>
           <button className='mt-5 m-2 w-14 h-14 flex items-center justify-center bg-orange-500 rounded-lg'>)</button>
